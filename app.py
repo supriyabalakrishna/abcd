813155a7a8789bb9b4df925eb67fccee5f4bba0e
supriyabalakrishna <supriyabalakrishna598@gmail.com>
2025-08-12 21:40:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -110,17 +110,8 @@ def download():
     df_cleaned.to_csv(output, index=False)
     output.seek(0)
     return send_file(output, as_attachment=True, download_name="cleaned_dataset.csv")
-  import os
-from flask import Flask, render_template
 
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return render_template("index.html")
-
+# ---------- Run the app ----------
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
-
-
